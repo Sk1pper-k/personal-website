fc9107d6b23b4f9d29975172da1fcc01202d3eac
Rasith Senanayake <108359497+Sk1pper-k@users.noreply.github.com>
2023-04-08 12:27:50 +0530

    Add files via upload

---
+++ Web X/Untitled-2.py
@@ -0,0 +1,13 @@
+def product():
+    num1 = int(input("Enter number 1 - "))
+    num2 = int(input("Enter number 2 - "))
+    num3 = int(input("Enter number 3 - "))
+    func = input("Enter function type '+'= ")
+    if (func == "+"):
+        ans = num1 + num2 + num3
+        2
+        print("Incorrect function type")
+    print("\nThe answer is", ans)
+
+
+product()
